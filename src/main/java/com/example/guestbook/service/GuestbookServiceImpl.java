1f29b0a6678d5acc72f043399585266f0ec08dfb
SeungCheol <ssc6839@gmail.com>
2021-08-18 22:38:50 +0900

    방명록 조회 처리

--- src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -48,5 +49,13 @@ public class GuestbookServiceImpl implements GuestbookService{
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent() ? entityToDto(result.get()) : null;
     }
 }
